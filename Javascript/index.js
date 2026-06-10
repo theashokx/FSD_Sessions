136b832b207accf5c20af8b26862a5116e8c7cbd
theashokx <theashokx@gmail.com>
2026-06-10 14:09:04 +0530

    Done with spread,rest,Destructuring,promise

--- Javascript/index.js
+++ Javascript/index.js
@@ -23,162 +23,162 @@
 // arr.shift();
 // console.log(arr);
 
-let arr = [1, 2, 3, 4, 5];
+// let arr = [1, 2, 3, 4, 5];
 
-console.log("Original:", arr);
+// console.log("Original:", arr);
 
-arr.push(6);
-console.log("push:", arr);
+// arr.push(6);
+// console.log("push:", arr);
 
-arr.pop();
-console.log("pop:", arr);
+// arr.pop();
+// console.log("pop:", arr);
 
-arr.unshift(0);
-console.log("unshift:", arr);
+// arr.unshift(0);
+// console.log("unshift:", arr);
 
-arr.shift();
-console.log("shift:", arr);
+// arr.shift();
+// console.log("shift:", arr);
 
-console.log("length:", arr.length);
+// console.log("length:", arr.length);
 
-console.log("indexOf(3):", arr.indexOf(3));
+// console.log("indexOf(3):", arr.indexOf(3));
 
-console.log("includes(4):", arr.includes(4));
+// console.log("includes(4):", arr.includes(4));
 
-let arr2 = [6, 7, 8];
-console.log("concat:", arr.concat(arr2));
+// let arr2 = [6, 7, 8];
+// console.log("concat:", arr.concat(arr2));
 
-console.log("join:", arr.join("-"));
+// console.log("join:", arr.join("-"));
 
-let rev = [...arr];
-rev.reverse();
-console.log("reverse:", rev);
+// let rev = [...arr];
+// rev.reverse();
+// console.log("reverse:", rev);
 
-let unsorted = [5, 2, 8, 1, 9];
-unsorted.sort((a, b) => a - b);
-console.log("sort:", unsorted);
+// let unsorted = [5, 2, 8, 1, 9];
+// unsorted.sort((a, b) => a - b);
+// console.log("sort:", unsorted);
 
-console.log("slice:", arr.slice(1, 4));
+// console.log("slice:", arr.slice(1, 4));
 
-let spliceArr = [...arr];
-spliceArr.splice(1, 2);
-console.log("splice:", spliceArr);
+// let spliceArr = [...arr];
+// spliceArr.splice(1, 2);
+// console.log("splice:", spliceArr);
 
-let fillArr = [1, 2, 3, 4];
-fillArr.fill(0);
-console.log("fill:", fillArr);
+// let fillArr = [1, 2, 3, 4];
+// fillArr.fill(0);
+// console.log("fill:", fillArr);
 
-arr.forEach((num) => console.log("forEach:", num));
+// arr.forEach((num) => console.log("forEach:", num));
 
-console.log(
-  "map:",
-  arr.map((num) => num * 2),
-);
+// console.log(
+//   "map:",
+//   arr.map((num) => num * 2),
+// );
 
-console.log(
-  "filter:",
-  arr.filter((num) => num % 2 === 0),
-);
+// console.log(
+//   "filter:",
+//   arr.filter((num) => num % 2 === 0),
+// );
 
-console.log(
-  "find:",
-  arr.find((num) => num > 3),
-);
+// console.log(
+//   "find:",
+//   arr.find((num) => num > 3),
+// );
 
-console.log(
-  "findIndex:",
-  arr.findIndex((num) => num > 3),
-);
+// console.log(
+//   "findIndex:",
+//   arr.findIndex((num) => num > 3),
+// );
 
-console.log(
-  "every:",
-  arr.every((num) => num > 0),
-);
+// console.log(
+//   "every:",
+//   arr.every((num) => num > 0),
+// );
 
-console.log(
-  "some:",
-  arr.some((num) => num > 4),
-);
+// console.log(
+//   "some:",
+//   arr.some((num) => num > 4),
+// );
 
-console.log(
-  "reduce:",
-  arr.reduce((acc, curr) => acc + curr, 0),
-);
+// console.log(
+//   "reduce:",
+//   arr.reduce((acc, curr) => acc + curr, 0),
+// );
 
-let nested = [1, [2, 3], [4, [5]]];
-console.log("flat:", nested.flat());
+// let nested = [1, [2, 3], [4, [5]]];
+// console.log("flat:", nested.flat());
 
-console.log(
-  "flatMap:",
-  arr.flatMap((x) => [x, x * 2]),
-);
+// console.log(
+//   "flatMap:",
+//   arr.flatMap((x) => [x, x * 2]),
+// );
 
-console.log("Array.from:", Array.from("hello"));
+// console.log("Array.from:", Array.from("hello"));
 
-console.log("Array.isArray:", Array.isArray(arr));
+// console.log("Array.isArray:", Array.isArray(arr));
 
-console.log("at(-1):", arr.at(-1));
+// console.log("at(-1):", arr.at(-1));
 
-let str = "Hello World";
+// let str = "Hello World";
 
-console.log("Original:", str);
+// console.log("Original:", str);
 
-console.log("length:", str.length);
+// console.log("length:", str.length);
 
-console.log("charAt(1):", str.charAt(1));
+// console.log("charAt(1):", str.charAt(1));
 
-console.log("at(-1):", str.at(-1));
+// console.log("at(-1):", str.at(-1));
 
-console.log("indexOf('o'):", str.indexOf("o"));
+// console.log("indexOf('o'):", str.indexOf("o"));
 
-console.log("lastIndexOf('o'):", str.lastIndexOf("o"));
+// console.log("lastIndexOf('o'):", str.lastIndexOf("o"));
 
-console.log("includes('World'):", str.includes("World"));
+// console.log("includes('World'):", str.includes("World"));
 
-console.log("startsWith('Hello'):", str.startsWith("Hello"));
+// console.log("startsWith('Hello'):", str.startsWith("Hello"));
 
-console.log("endsWith('World'):", str.endsWith("World"));
+// console.log("endsWith('World'):", str.endsWith("World"));
 
-console.log("toUpperCase():", str.toUpperCase());
+// console.log("toUpperCase():", str.toUpperCase());
 
-console.log("toLowerCase():", str.toLowerCase());
+// console.log("toLowerCase():", str.toLowerCase());
 
-console.log("slice(0,5):", str.slice(0, 5));
+// console.log("slice(0,5):", str.slice(0, 5));
 
-console.log("substring(0,5):", str.substring(0, 5));
+// console.log("substring(0,5):", str.substring(0, 5));
 
-console.log(
-  "replace('World','JavaScript'):",
-  str.replace("World", "JavaScript"),
-);
+// console.log(
+//   "replace('World','JavaScript'):",
+//   str.replace("World", "JavaScript"),
+// );
 
-console.log("replaceAll('l','L'):", str.replaceAll("l", "L"));
+// console.log("replaceAll('l','L'):", str.replaceAll("l", "L"));
 
-console.log("split(' '):", str.split(" "));
+// console.log("split(' '):", str.split(" "));
 
-console.log("concat():", str.concat("!!!"));
+// console.log("concat():", str.concat("!!!"));
 
-console.log("repeat(3):", str.repeat(3));
+// console.log("repeat(3):", str.repeat(3));
 
-console.log("trim():", "   Hello   ".trim());
+// console.log("trim():", "   Hello   ".trim());
 
-console.log("trimStart():", "   Hello".trimStart());
+// console.log("trimStart():", "   Hello".trimStart());
 
-console.log("trimEnd():", "Hello   ".trimEnd());
+// console.log("trimEnd():", "Hello   ".trimEnd());
 
-console.log("padStart(15,'*'):", str.padStart(15, "*"));
+// console.log("padStart(15,'*'):", str.padStart(15, "*"));
 
-console.log("padEnd(15,'*'):", str.padEnd(15, "*"));
+// console.log("padEnd(15,'*'):", str.padEnd(15, "*"));
 
-console.log("search('World'):", str.search("World"));
+// console.log("search('World'):", str.search("World"));
 
-console.log("match(/o/g):", str.match(/o/g));
+// console.log("match(/o/g):", str.match(/o/g));
 
-console.log("localeCompare('Hello'):", str.localeCompare("Hello"));
+// console.log("localeCompare('Hello'):", str.localeCompare("Hello"));
 
-console.log("valueOf():", str.valueOf());
+// console.log("valueOf():", str.valueOf());
 
-console.log("String.fromCharCode(65):", String.fromCharCode(65));
+// console.log("String.fromCharCode(65):", String.fromCharCode(65));
 
 // function token() {
 //   setTimeout(() => {
@@ -247,3 +247,90 @@ console.log("String.fromCharCode(65):", String.fromCharCode(65));
 //     });
 //   });
 // });
+
+// function add(...rest) {
+//   for (let i = 0; i < rest.length; i++) {
+//     console.log(i + "---" + rest[i]);
+//   }
+// }
+// let arr = [1, 2, 3, 4, 5];
+// console.log(add(0, ...arr));
+
+// const [a, b, c] = [1, 2, 3];
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// const obj = {
+//   name: "Ashok",
+//   rollno: 11,
+//   age: 20,
+//   phone: {
+//     cell: 9876543210,
+//     mobile: 9087654321,
+//   },
+// };
+
+// const {
+//   name,
+//   age,
+//   phone: { cell, mobile },
+// } = obj;
+// console.log(name);
+// console.log(age);
+// console.log(mobile);
+// console.log(cell);
+
+// localStorage.setItem("name", "Surya");
+// localStorage.setItem("age", "20");
+
+// console.log(localStorage.getItem("name"));
+// console.log(localStorage.getItem("age"));
+
+// localStorage.removeItem("age");
+// localStorage.clear();
+
+// sessionStorage.setItem("user", "Surya");
+// console.log(sessionStorage.getItem("user"));
+// sessionStorage.clear();
+// sessionStorage.setItem("course", "React");
+// console.log(sessionStorage.getItem("course"));
+
+// let p = new Promise((res, rej) => {
+//   let val = false;
+//   if (val) {
+//     return res("Sucessfully completed");
+//   }
+//   return rej("Sorry Failed");
+// });
+
+// p.then((m) => {
+//   console.log(m);
+// }).catch((err) => {
+//   console.log(err);
+// });
+
+// function orderFood() {
+//   return new Promise((resolve, reject) => {
+//     console.log("Idly is ordered");
+//     resolve("Idly");
+//   });
+// }
+
+// function preparedFood(food) {
+//   return new Promise((resolve, reject) => {
+//     console.log(`${food} is prepared`);
+//     resolve(`${food} is prepared`);
+//   });
+// }
+
+// function deliveredOrder(food) {
+//   return new Promise((resolve, reject) => {
+//     console.log(`${food} is delivered`);
+//   });
+// }
+
+// orderFood()
+//   .then((food) => preparedFood(food))
+//   .then((food) => deliveredOrder(food))
+//   .catch((err) => console.log(err));
