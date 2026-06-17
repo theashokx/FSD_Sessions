806cebc914936c702a45db30bd3e6d72abfc736f
theashokx <theashokx@gmail.com>
2026-06-17 21:01:03 +0530

    Feat(backend-2):Done with middlewares

--- backend-1/index.js
+++ backend-1/index.js
@@ -1,117 +1,171 @@
-const express = require("express");
-const app = express();
-const users = require("./User.json");
-const fs = require("fs");
+// const express = require("express");
+// const app = express();
+// const users = require("./User.json");
+// const fs = require("fs");
 
-app.use(express.json());
+// app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.status(200).send("Hello ");
-//   res.send("world");
+// // app.get("/", (req, res) => {
+// //   res.status(200).send("Hello ");
+// //   res.send("world");
+// // });
+
+// // app.get("/user", (req, res) => {
+// //   res.status(200).json({ message: "Sucessfull" });
+// // });
+// // app.get("/user/:id", (req, res) => {
+// //   console.log(req.params);
+// //   console.log(req.query);
+// //   res.status(200).sendFile(__dirname + "/index.html");
+// // });
+
+// app.get("/users", (req, res) => {
+//   //const html = users.map((user) => `<li>${user.name}</li>`).join(" ");
+//   //   res.status(200).send(`
+//   //     <div>
+//   //       <ul>${html}</ul>
+//   //     </div>`);
+//   res.status(200).json(users);
 // });
 
-// app.get("/user", (req, res) => {
-//   res.status(200).json({ message: "Sucessfull" });
+// app.post("/users", (req, res) => {
+//   const newUser = req.body;
+//   console.log(newUser);
+//   users.push(newUser);
+//   fs.writeFile("./User.json", JSON.stringify(users, null, 2), (err) => {
+//     if (err) {
+//       return res.status(500).send("Error saving user");
+//     }
+
+//     res.status(200).json(newUser);
+//   });
 // });
-// app.get("/user/:id", (req, res) => {
-//   console.log(req.params);
-//   console.log(req.query);
-//   res.status(200).sendFile(__dirname + "/index.html");
+
+// app.put("/users/:id", async (req, res) => {
+//   const id = Number(req.params.id);
+
+//   const idx = users.findIndex((user) => user.id === id);
+
+//   if (idx === -1) {
+//     return res.status(404).json({
+//       message: "User not found",
+//     });
+//   }
+
+//   const updatedUser = {
+//     id,
+//     ...req.body,
+//   };
+
+//   users[idx] = updatedUser;
+
+//   await fs.writeFile("./User.json", JSON.stringify(users, null, 2), (err) => {
+//     if (err) {
+//       return res.status(500).send("Error saving user");
+//     }
+
+//     res.status(200).json(updatedUser);
+//   });
 // });
 
-app.get("/users", (req, res) => {
-  //const html = users.map((user) => `<li>${user.name}</li>`).join(" ");
-  //   res.status(200).send(`
-  //     <div>
-  //       <ul>${html}</ul>
-  //     </div>`);
-  res.status(200).json(users);
-});
+// app.delete("/users/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   const idx = users.findIndex((user) => user.id === id);
+//   const user = users[idx];
+//   if (idx === -1) {
+//     return res.status(400).send("User not available");
+//   }
+//   users.splice(idx, 1);
+//   console.log("deleted sucessfully");
+//   fs.writeFile("./User.json", JSON.stringify(users, null, 2), (err) => {
+//     if (err) {
+//       return res.status(400).send("Unable to delete");
+//     }
+//     return res.status(200).json({ message: "sucessfully deleted", user });
+//   });
+// });
 
-app.post("/users", (req, res) => {
-  const newUser = req.body;
-  console.log(newUser);
-  users.push(newUser);
-  fs.writeFile("./User.json", JSON.stringify(users, null, 2), (err) => {
-    if (err) {
-      return res.status(500).send("Error saving user");
-    }
+// app.patch("/users/:id", (req, res) => {
+//   const id = Number(req.params.id);
 
-    res.status(200).json(newUser);
-  });
-});
+//   const idx = users.findIndex((user) => user.id === id);
 
-app.put("/users/:id", async (req, res) => {
-  const id = Number(req.params.id);
+//   if (idx === -1) {
+//     return res.status(404).json({
+//       message: "User not found",
+//     });
+//   }
 
-  const idx = users.findIndex((user) => user.id === id);
+//   users[idx] = {
+//     ...users[idx],
+//     ...req.body,
+//   };
 
-  if (idx === -1) {
-    return res.status(404).json({
-      message: "User not found",
-    });
-  }
+//   fs.writeFile("./User.json", JSON.stringify(users, null, 2), (err) => {
+//     if (err) {
+//       return res.status(500).json({
+//         message: "Error updating user",
+//       });
+//     }
 
-  const updatedUser = {
-    id,
-    ...req.body,
-  };
+//     res.status(200).json(users[idx]);
+//   });
+// });
 
-  users[idx] = updatedUser;
+// app.listen(3000, () => {
+//   console.log("Server running sucessfully");
+// });
 
-  await fs.writeFile("./User.json", JSON.stringify(users, null, 2), (err) => {
-    if (err) {
-      return res.status(500).send("Error saving user");
-    }
-
-    res.status(200).json(updatedUser);
-  });
-});
-
-app.delete("/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const idx = users.findIndex((user) => user.id === id);
-  const user = users[idx];
-  if (idx === -1) {
-    return res.status(400).send("User not available");
-  }
-  users.splice(idx, 1);
-  console.log("deleted sucessfully");
-  fs.writeFile("./User.json", JSON.stringify(users, null, 2), (err) => {
-    if (err) {
-      return res.status(400).send("Unable to delete");
-    }
-    return res.status(200).json({ message: "sucessfully deleted", user });
-  });
-});
-
-app.patch("/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-
-  const idx = users.findIndex((user) => user.id === id);
-
-  if (idx === -1) {
-    return res.status(404).json({
-      message: "User not found",
-    });
-  }
-
-  users[idx] = {
-    ...users[idx],
-    ...req.body,
-  };
-
-  fs.writeFile("./User.json", JSON.stringify(users, null, 2), (err) => {
-    if (err) {
-      return res.status(500).json({
-        message: "Error updating user",
-      });
-    }
-
-    res.status(200).json(users[idx]);
-  });
-});
-
-app.listen(3000, () => {
-  console.log("Server running sucessfully");
-});
+// const express = require("express");
+// const fs = require("fs");
+// const app = express();
+// const users = require("./User.json");
+// app.use(express.json());
+// app.use((req, res, next) => {
+//   console.log(`${req.method} ${req.url}`);
+//   next();
+// });
+// const validateUser = (req, res, next) => {
+//   if (!req.body.name)
+//     return res.status(400).json({ message: "Name is required" });
+//   next();
+// };
+// const checkUserExists = (req, res, next) => {
+//   const id = Number(req.params.id);
+//   const idx = users.findIndex((user) => user.id === id);
+//   if (idx === -1) return res.status(404).json({ message: "User not found" });
+//   req.userIndex = idx;
+//   next();
+// };
+// app.get("/users", (req, res) => {
+//   res.json(users);
+// });
+// app.get("/users/:id", checkUserExists, (req, res) => {
+//   res.json(users[req.userIndex]);
+// });
+// app.post("/users", validateUser, (req, res) => {
+//   const newUser = {
+//     id: users.length ? Math.max(...users.map((u) => u.id)) + 1 : 1,
+//     ...req.body,
+//   };
+//   users.push(newUser);
+//   fs.writeFile("./User.json", JSON.stringify(users, null, 2), (err) => {
+//     if (err) return res.status(500).json({ message: "Error saving user" });
+//     res.status(201).json(newUser);
+//   });
+// });
+// app.patch("/users/:id", checkUserExists, (req, res) => {
+//   users[req.userIndex] = { ...users[req.userIndex], ...req.body };
+//   fs.writeFile("./User.json", JSON.stringify(users, null, 2), (err) => {
+//     if (err) return res.status(500).json({ message: "Error updating user" });
+//     res.json(users[req.userIndex]);
+//   });
+// });
+// app.delete("/users/:id", checkUserExists, (req, res) => {
+//   const deleted = users.splice(req.userIndex, 1);
+//   fs.writeFile("./User.json", JSON.stringify(users, null, 2), (err) => {
+//     if (err) return res.status(500).json({ message: "Error deleting user" });
+//     res.json({ message: "Deleted successfully", user: deleted[0] });
+//   });
+// });
+// app.listen(3000, () => console.log("Server running on port 3000"));
